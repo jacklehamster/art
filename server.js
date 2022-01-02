9ecc2e4763b6022ef40681a5beb20be4d1b6b79a
Vincent Le Quang <vinceq@amazon.com>
2022-01-02 15:29:45 -0800

    ccr

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ var server = http.createServer(function(request, response) {
   var filePath = false;
 
   if (request.url == '/') {
-    filePath = "public/index.html";
+    filePath = "index.html";
   } else {
-    filePath = "public" + request.url;
+    filePath = request.url;
   }
 
   var absPath = "./" + filePath;
